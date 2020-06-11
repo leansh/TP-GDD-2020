17676531c29bcd42547e7d521b849c897cc66c8b
Leandro Shokida <leandroshokida@gmail.com>
2020-06-11 18:22:11 -0300

    commit inicial

---
+++ drop.sql
@@ -0,0 +1,21 @@
+USE GD1C2020
+GO
+drop table CUARENTENA2020.Estadia_X_Habitacion
+drop table CUARENTENA2020.VentaEstadia
+drop table CUARENTENA2020.CompraEstadia
+drop table CUARENTENA2020.VentaPasaje
+drop table CUARENTENA2020.Pasaje
+drop table CUARENTENA2020.Butaca 
+drop table CUARENTENA2020.Estadia
+drop table CUARENTENA2020.Habitacion
+drop table CUARENTENA2020.Venta
+drop table CUARENTENA2020.CompraPasaje
+drop table CUARENTENA2020.Vuelo 
+drop table CUARENTENA2020.Cliente
+drop table CUARENTENA2020.Factura
+drop table CUARENTENA2020.Compra
+drop table CUARENTENA2020.Avion 
+drop table CUARENTENA2020.Ruta 
+drop table CUARENTENA2020.Hotel 
+drop table CUARENTENA2020.Empresa 
+drop table CUARENTENA2020.Sucursal
